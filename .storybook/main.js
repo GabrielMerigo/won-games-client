1a8413d39313baa153470ac206f910148ce4af84
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-04-28 13:58:15 -0300

    more accs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,9 @@
 module.exports = {
   "stories": ["../src/components/**/stories.tsx"],
   "addons": ["@storybook/addon-essentials"],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config;
+  },
 }
---
+++ src/components/Logo/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, Story } from '@storybook/react';
+import Logo from '.';
+
+export default {
+  title: `Logo`,
+  component: Logo
+} as Meta
+
+export const LogoStory: Story = () => <Logo />
