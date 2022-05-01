34fc110ee6fd8465787846f314d0a3dc07b8065f
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-04-30 21:41:14 -0300

    finalizando testes do componente logo

--- src/components/Logo/test.spec.tsx
+++ src/components/Logo/test.spec.tsx
@@ -1,27 +1,45 @@
-import { render, screen } from '@testing-library/react'
-import { renderWithTheme } from '../../utils/tests/helpers'
+import { render, screen } from '@testing-library/react';
+import { renderWithTheme } from '../../utils/tests/helpers';
+import 'jest-styled-components';
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('Should render a white label by default', () => {
-    renderWithTheme(<Logo />);
-    expect(screen.getByAltText(/Won Games/i).parentElement).toHaveStyle({
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#FAFAFA'
     })
   })
 
-  it('Should render a black label when color is passed', () => {
-    renderWithTheme(<Logo color='black' />);
+  it('should render a black label', () => {
+    renderWithTheme(<Logo color="black" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517'
     })
   })
 
-  it('Should render a bigger logo', () => {
-    renderWithTheme(<Logo size="large"/>);
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '20rem'
     })
+  })
+
+  it('should render a normal logo by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      })
   })
 })
