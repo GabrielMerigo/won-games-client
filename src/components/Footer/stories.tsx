40f01a7c91d2ddec5a92fbd0de2ac45ad223b81c
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-05-10 12:12:42 -0300

    mais testes

---
+++ src/components/Footer/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, Story } from '@storybook/react';
+import Footer from '.';
+
+export default {
+  title: `Footer`,
+  component: Footer
+} as Meta
+
+export const Footer: Story = () => <Footer />
