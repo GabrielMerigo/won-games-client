52327807f1fb5b262db22850623b48c2dd31f872
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-05-13 00:53:40 -0300

    estilizando

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,7 +1,9 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query';
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.main`
+  position: relative;
+`
 
 type ImageProps = {
   src: string;
@@ -22,8 +24,48 @@ export const Image = styled.div<ImageProps>`
   `}
 `
 
-export const Caption = styled.div``
+export const Caption = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    padding: ${theme.spacings.small};
 
-export const Title = styled.h2``
+    ${media.greaterThan('medium')`
+      border-radius: 0 0 ${theme.border.radius} ${theme.border.radius};
+      padding: ${theme.spacings.large};
+      position: absolute;
+      bottom: 0;
+      left: 0;
+    `}
+  `}
+`
 
-export const Subtitle = styled.h3``
+export const Title = styled.h2`
+   ${({ theme }) => css`
+    font-size: ${theme.font.sizes.large};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.white};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge};
+    `}
+  `}
+`
+
+export const Subtitle = styled.h3`
+   ${({ theme }) => css`
+    color: ${theme.colors.white};
+    font-size: ${theme.font.sizes.small};
+    font-weight: ${theme.font.normal};
+    margin-bottom: ${theme.spacings.xsmall};
+
+    strong {
+      color: ${theme.colors.primary};
+      font-weight: ${theme.font.bold};
+    }
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.large};
+    `}
+  `}
+`
