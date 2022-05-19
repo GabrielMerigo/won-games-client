1a90c0de9ee913306c57079a14cdfac33324a91b
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-05-19 18:54:50 -0300

    more accs

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,8 +1,16 @@
 import * as S from './styles'
+import SlickSlider, { Settings } from "react-slick";
 
-const Slider = () => (
+export type SliderSettings = Settings
+
+export type SliderProps = {
+  children: React.ReactNode
+  settings: SliderSettings
+}
+
+const Slider = ({ children, settings }: SliderProps) => (
   <S.Wrapper>
-    <h1>Slider</h1>
+    <SlickSlider {...settings}>{children}</SlickSlider>
   </S.Wrapper>
 )
 
