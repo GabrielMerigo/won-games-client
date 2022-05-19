3067d1807637b4193060b17dbbec70de2f85f67c
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-05-19 16:44:09 -0300

    more accs

---
+++ src/components/Slider/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, Story } from '@storybook/react';
+import Slider from '.';
+
+export default {
+  title: `Slider`,
+  component: Slider
+} as Meta
+
+export const SliderStory: Story = () => <Slider />
