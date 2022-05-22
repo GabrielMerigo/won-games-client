eb150796d5d7ab57345993be7f2c00315f6255b8
gabriel.merigo <gabriel.merigo@somosquiq.com>
2022-05-21 23:27:39 -0300

    criando componente container

---
+++ src/components/Container/test.spec.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import theme from "../../styles/theme";
+import { renderWithTheme } from "../../utils/tests/helpers";
+
+import { Container } from "./";
+
+describe("<Container />", () => {
+  it("should have 130rem as width", () => {
+    const { container } = renderWithTheme(<Container />);
+
+    expect(container.firstChild).toHaveStyle({
+      "max-width": theme.grid.container,
+    });
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        max-width: 130rem;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: calc(3.2rem / 2);
+        padding-right: calc(3.2rem / 2);
+      }
+
+      <div
+        class="c0"
+      />
+    `);
+  });
+});
