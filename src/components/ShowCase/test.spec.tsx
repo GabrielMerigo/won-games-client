18986a3b883335954d48b930e51833a458b71e5c
Gabriel <gabriel.merigo.dev@gmail.com>
2022-06-20 17:17:22 -0300

    show case criado, falta os testes

--- src/components/ShowCase/test.spec.tsx
+++ src/components/ShowCase/test.spec.tsx
@@ -3,5 +3,8 @@ import { render, screen } from '@testing-library/react'
 import ShowCase from './'
 
 describe('<ShowCase />', () => {
+  //Quando tiver titulo deve aparacer titulos
+  // quando tiver highlight deve aparecer highlight
+  // Quando tiver heading deve aparecer heading
   it('', () => {})
 })
