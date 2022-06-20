39db04399e0864301884a75edcf1b256f858c972
Gabriel <gabriel.merigo.dev@gmail.com>
2022-06-20 19:15:53 -0300

    tests finished

--- src/components/ShowCase/test.spec.tsx
+++ src/components/ShowCase/test.spec.tsx
@@ -1,10 +1,88 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components';
+import '../../../.jest/match-media-mock';
 
 import ShowCase from './'
 
+const propsHighlight = {
+  title: 'Heading 1',
+  subtitle: 'Heading 2',
+  buttonLabel: 'Buy Now',
+  buttonLink: '/rdr2',
+  backgroundImage: 'img/img-rd2.jpg',
+  floatImage: 'img/float-image.jpg'
+}
+
+const items = [
+  {
+    title: 'Population Zero 1',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero 2',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero 3',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero 4',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero 5',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero 6',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x145',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  }
+]
+
 describe('<ShowCase />', () => {
-  //Quando tiver titulo deve aparacer titulos
-  // quando tiver highlight deve aparecer highlight
   // Quando tiver heading deve aparecer heading
-  it('', () => {})
+  it('should render title when property title is passed', () => {
+    renderWithTheme(<ShowCase title="Up comming" highlight={propsHighlight} games={items} />)
+    expect(screen.getByText(/Up comming/i)).toBeInTheDocument();
+    expect(screen.getByRole('heading', {  name: 'Heading 1' })).toBeInTheDocument();
+    expect(document.querySelectorAll('.slick-slide')).toHaveLength(6);
+  });
+
+  it('should not render title when property title is not passed', () => {
+    renderWithTheme(<ShowCase />)
+    expect(screen.queryByText(/Up comming/i)).not.toBeInTheDocument();
+  });
+
+
+  it('should not highlight title when property highlight is not passed', () => {
+    renderWithTheme(<ShowCase />)
+    expect(screen.queryByRole('heading', { name: propsHighlight.title })).not.toBeInTheDocument();
+  });
+
+
+  it('should not render games when property games is not passed', () => {
+    renderWithTheme(<ShowCase />)
+    expect(document.querySelectorAll('.slick-slide')).not.toHaveLength(6);
+    expect(screen.queryByRole('heading', { name: items[0].title })).not.toBeInTheDocument();
+  });
 })
