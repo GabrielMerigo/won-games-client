db9576e3b707d8df87398cae8adbcbae1a24e62b
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-07-16 05:04:31 -0300

    Update styles.ts

--- src/components/GameItem/styles.ts
+++ src/components/GameItem/styles.ts
@@ -1,10 +1,41 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    padding: ${theme.spacings.small};
+    border-bottom: 0.1rem solid ${theme.colors.lightGray};
 
-export const GameContent = styled.div``
+    ${media.greaterThan('medium')`
 
-export const ImageBox = styled.div``
+    `}
+  `}
+`
+
+export const GameContent = styled.div`
+  display: flex;
+`
+
+export const ImageBox = styled.div`
+  ${({ theme }) => css`
+    flex-shrink: 0;
+    margin-right: 1.2rem;
+    width: 9.6rem;
+    height: 5.6rem;
+
+    img{
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    ${media.greaterThan('medium')`
+      margin-right: ${theme.spacings.xsmall};
+      width: 15rem;
+      height: 7rem;
+    `}
+  `}
+`
 
 export const Content = styled.div``
 
