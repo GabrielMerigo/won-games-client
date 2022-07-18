15e2f519b8311e9cdcd37e00e847cd3f9a95def0
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-07-18 18:20:01 -0300

    more accs

--- src/components/PaymentOptions/test.spec.tsx
+++ src/components/PaymentOptions/test.spec.tsx
@@ -1,7 +1,15 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import mockCards from './mock';
 
 import PaymentOptions from './'
 
 describe('<PaymentOptions />', () => {
-  it('', () => {})
+  it('should render the saved card options and the add new card options', () => {
+    renderWithTheme(<PaymentOptions cards={mockCards} handlePayment={jest.fn} />);
+
+    expect(screen.getByLabelText(/4325/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/4326/i)).toBeInTheDocument();
+    expect(screen.getByText(/add a new credit card/i)).toBeInTheDocument();
+  });
 })
