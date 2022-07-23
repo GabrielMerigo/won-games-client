a99b801894248d70ea6054db8090b736558d07b2
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-07-23 20:38:44 -0300

    profile finished

--- src/templates/Profile/index.tsx
+++ src/templates/Profile/index.tsx
@@ -1,27 +1,32 @@
 import { Container } from 'components/Container'
 import React from 'react';
 import Heading from '../../components/Heading'
-import ProfileMenu from '../../components/ProfileMenu';
+import ProfileMenu, { ProfileMenuProps } from '../../components/ProfileMenu';
 import Base from '../../templates/Base'
 import * as S from './styles';
+import { useRouter } from 'next/router';
 
 export type ProfileTemplateProps = {
   children: React.ReactNode;
 }
 
-const Profile = ({ children }: ProfileTemplateProps) => (
-  <Base>
-    <Container>
-      <Heading lineLeft lineColor='secondary'>My Profile</Heading>
+const Profile = ({ children }: ProfileTemplateProps) => {
+  const { pathname } = useRouter();
 
-      <S.Main>
-        <ProfileMenu />
-        <S.Content>
-          {children}
-        </S.Content>
-      </S.Main>
-    </Container>
-  </Base>
-)
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor='secondary'>My Profile</Heading>
+
+        <S.Main>
+          <ProfileMenu activeLink={(pathname as "/profile/me" | "/profile/cards" | "/profile/orders")} />
+          <S.Content>
+            {children}
+          </S.Content>
+        </S.Main>
+      </Container>
+    </Base>
+  )
+}
 
 export default Profile
