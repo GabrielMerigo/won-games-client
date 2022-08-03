a0c68d26da0e894378408702c190b02378102d1e
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-08-03 07:37:53 -0300

    Update test.spec.tsx

--- src/templates/Games/test.spec.tsx
+++ src/templates/Games/test.spec.tsx
@@ -1,7 +1,20 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers';
+import filterItemsMock from '../../components/ExploreSidebar/mock';
 
 import Games from '.'
 
+jest.mock('../../components/ExploreSidebar', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock ExploreSidebar"></div>
+    }
+  }
+});
+
 describe('<Games />', () => {
-  it('', () => { })
+  it('should render sidebar, games cards and button show more ', () => {
+    renderWithTheme(<Games filterItems={filterItemsMock} />)
+  })
 })
