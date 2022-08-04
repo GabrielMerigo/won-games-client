3b7c5c64a57f0ca791b6ca00cafbf7d8424ea89d
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-08-04 08:53:36 -0300

    more accs

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -24,8 +24,8 @@ const Checkbox = ({
   const onChange = () => {
     const status = !checked;
     setChecked(status);
-    console.log(status)
-    if(onCheck){
+
+    if (onCheck) {
       onCheck(status);
     }
   }
