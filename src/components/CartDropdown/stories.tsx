c052d7a7000bc8ee7c5f7c971872e8d080c31158
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-08-04 09:37:58 -0300

    more accs

---
+++ src/components/CartDropdown/stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, Story } from '@storybook/react';
+import CartDropdown, { CartDropdownProps } from '.';
+
+import itemsMock from 'components/CartList/mock';
+
+export default {
+  title: `CartDropdown`,
+  component: CartDropdown,
+  args: {
+    items: itemsMock,
+    total: 'R$ 300,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<CartDropdownProps> = (args) => (
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
+    <CartDropdown {...args} />
+  </div>
+)
