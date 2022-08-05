9462049e57398ef1673aaf9419cb107858cafe18
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-08-05 10:58:55 -0300

    more accs

--- src/components/CartList/test.spec.tsx
+++ src/components/CartList/test.spec.tsx
@@ -7,7 +7,6 @@ import mockItems from './mock';
 describe('<CartList />', () => {
   it('should render the cart list', () => {
     const { container } = renderWithTheme(<CartList items={mockItems} total="R$ 330,00" />);
-
     expect(screen.getAllByRole('img')).toHaveLength(2)
     expect(screen.getByText(/R\$ 330,00/i)).toBeInTheDocument();
     expect(screen.getByText(/Total/i)).toBeInTheDocument();
