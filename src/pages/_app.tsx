824a03e2658fe8da105b22b517bf4eeb2be730e6
GabrielMerigo <gabriel.merigo.dev@gmail.com>
2022-08-20 14:02:06 -0300

    using apollo client with useQuery

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,29 @@ import Head from 'next/head'
 import GlobalStyle from '../styles/global'
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 function App({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache()
+  })
+
+
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/Logo.png" />
-        <link rel="apple-touch-icon" href="/img/Logo.png" />
-        <meta name="description" content="A simple project" />
-        <link rel='manifest' href='/manifest.json' />
-      </Head>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <link rel="shortcut icon" href="/img/Logo.png" />
+          <link rel="apple-touch-icon" href="/img/Logo.png" />
+          <meta name="description" content="A simple project" />
+          <link rel='manifest' href='/manifest.json' />
+        </Head>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,42 @@ import Home, { HomeTemplateProps } from '../templates/Home'
 import bannersMock from '../components/BannerSlider/mock';
 import gamesMock from '../components/GameCardSlider/mock'
 import highlightMock from '../components/Highlight/mock'
+import { gql, useQuery } from '@apollo/client';
 
 export default function Index(props: HomeTemplateProps) {
+  const { data: response } = useQuery(gql`
+    query getCategories {
+        categories {
+          data {
+            attributes {
+              name
+              slug
+            }
+		      }
+        }
+      }
+  `)
+
+  console.log(response.categories.data)
+
+
   return (
     <Home {...props} />
   );
 }
 
-export function getServerSideProps(){
+export function getServerSideProps() {
   return {
     props: {
       banners: bannersMock,
       newGames: gamesMock,
-      mostPopularHighlight:highlightMock,
+      mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
       upcomingGames: gamesMock,
       upcomingHighlight: highlightMock,
       upcomingMoreGames: gamesMock,
       freeGames: gamesMock,
-      freeHighlight:highlightMock
+      freeHighlight: highlightMock
     }
   }
 }
