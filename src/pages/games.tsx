b32a99922c83380d12068295f6a7918c34b699a4
GabrielMerigo <gabriel.merigo.dev@gmail.com>
2022-08-27 21:37:50 -0300

    app quase pronto

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,18 +1,47 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import GamesTemplate, { GamesTemplateProps } from '../templates/Games'
 import filterItemsMock from '../components/ExploreSidebar/mock';
 import gamesMock from 'components/GameCardSlider/mock';
+import { initializeApollo } from "utils/apollo";
+import { gql } from "@apollo/client";
+import { useCallback, useEffect } from "react";
 
 export default function GamesPage({ games, filterItems }: GamesTemplateProps) {
+  const apolloClient = initializeApollo();
+
+
+  useEffect(useCallback(async () => {
+    const data = await apolloClient.query({ query: gql`
+    query QueryGames {
+      games {
+        data {
+          id
+        }
+      }
+    }
+  `})
+  }), [])
+
   return <GamesTemplate games={games} filterItems={filterItems} />
 }
 
-// Não faz sentido essa página ser estática pois ela irá mudar o tempo todo
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticSideProps: GetStaticProps = async () => {
+
+
   return {
     props: {
-      games: gamesMock,
+      // games: data,
       filterItems: filterItemsMock
     }
   }
 }
+
+// {
+//   title: 'Cyberpunk',
+//   developer: 'Rockstar Games',
+//   img: 'https://source.unsplash.com/user/willianjusten/300x140',
+//   price: 'R$ 235,00',
+//   promotionalPrice: 'R$ 215,00',
+//   ribbon: '10% OFF',
+//   ribbonSize: ribbonSize
+// },
